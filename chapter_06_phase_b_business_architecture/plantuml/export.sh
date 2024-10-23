09a3134acd3004d10cfc00abd0fda07624799fd0
alfa-ryano <ary506@york.ac.uk>
2024-10-23 18:40:47 +0700

    Update Diagram

---
+++ chapter_06_phase_b_business_architecture/plantuml/export.sh
@@ -0,0 +1,4 @@
+# path to directory of plantuml
+cd ~/.vscode/extensions/jebbs.plantuml-2.18.1
+
+java -jar plantuml.jar -o "/data1/projects/enterprise-architecture/chapter_06_phase_b_business_architecture/plantuml/out/" "/data1/projects/enterprise-architecture/chapter_06_phase_b_business_architecture/plantuml/*" -pdf
