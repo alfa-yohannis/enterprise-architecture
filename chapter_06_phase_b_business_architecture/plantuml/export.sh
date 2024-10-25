e622158d2f8bad8c5bbf3b5068ec0967f3bbeef2
alfa-ryano <ary506@york.ac.uk>
2024-10-25 12:08:04 +0700

    Update

--- chapter_06_phase_b_business_architecture/plantuml/export.sh
+++ chapter_06_phase_b_business_architecture/plantuml/export.sh
@@ -1,4 +1,5 @@
 # path to directory of plantuml
 cd ~/.vscode/extensions/jebbs.plantuml-2.18.1
 
-java -jar plantuml.jar -o "/data1/projects/enterprise-architecture/chapter_06_phase_b_business_architecture/plantuml/out/" "/data1/projects/enterprise-architecture/chapter_06_phase_b_business_architecture/plantuml/*" -pdf
+java -jar plantuml.jar -o "/data1/projects/enterprise-architecture/chapter_06_phase_b_business_architecture/plantuml/out/" "/data1/projects/enterprise-architecture/chapter_06_phase_b_business_architecture/plantuml/*.puml" -pdf
+java -jar plantuml.jar -o "/data1/projects/enterprise-architecture/chapter_06_phase_b_business_architecture/plantuml/out/" "/data1/projects/enterprise-architecture/chapter_06_phase_b_business_architecture/plantuml/*.puml" -png
